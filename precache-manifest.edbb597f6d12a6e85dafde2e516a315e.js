c433f6b5d5bb4d627f8709d19020c33aca4e2b8d
JacyGames <bohdan.danultsiv@gmail.com>
2020-07-13 18:59:12 +0300

    Updates

---
+++ precache-manifest.edbb597f6d12a6e85dafde2e516a315e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0718325e48db7604cabb8d8cf3b95626",
+    "url": "/SocialNetwork-Samurai/index.html"
+  },
+  {
+    "revision": "84058283961b088984a6",
+    "url": "/SocialNetwork-Samurai/static/css/main.e42b9696.chunk.css"
+  },
+  {
+    "revision": "e701da7340d7cacab528",
+    "url": "/SocialNetwork-Samurai/static/js/2.9f6f02f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork-Samurai/static/js/2.9f6f02f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84058283961b088984a6",
+    "url": "/SocialNetwork-Samurai/static/js/main.98be03ca.chunk.js"
+  },
+  {
+    "revision": "5b09ace95e3e6da5e627",
+    "url": "/SocialNetwork-Samurai/static/js/runtime-main.f917dcc9.js"
+  },
+  {
+    "revision": "fe672064ae31dd4516b5bb9df5d35f41",
+    "url": "/SocialNetwork-Samurai/static/media/Logo.fe672064.png"
+  },
+  {
+    "revision": "fb6f99f760733ce2c47ee8f57668050b",
+    "url": "/SocialNetwork-Samurai/static/media/profilePic.fb6f99f7.jpg"
+  }
+]);
